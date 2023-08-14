9e0ff1623e2f841eab23bb1e39e963ef70bb86fb
@hexlet/cli <support@hexlet.io>
2023-08-14 18:15:36 +0300

    submit java-advanced-ru/multithreading-spring

---
+++ java-advanced-ru/multithreading-spring/src/main/java/exercise/controller/UsersController.java
@@ -0,0 +1,57 @@
+package exercise.controller;
+
+import exercise.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.http.MediaType;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import exercise.service.UserService;
+
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping(path = "")
+    public Flux<User> getUsers() {
+        return userService.findAll();
+    }
+
+    // BEGIN
+     @GetMapping(path = "/{id}")
+    public Mono<User> getUser(@PathVariable Long id) {
+        return userService.findById(id);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+                 produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<User> addUser(@RequestBody User user) {
+        return userService.create(user);
+    }
+
+    @PutMapping (consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<User> updateUser(@RequestBody User user) {
+        return userService.update(user);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public Mono<Void> deleteUser(@PathVariable Long id) {
+        return userService.delete(id);
+    }
+    // END
+}
